01010c79c3a503b93413414874d4fc4881c4ff44
X4R5 <91875972+X4R5@users.noreply.github.com>
2022-10-07 20:46:26 +0300

    Project Initialized

---
+++ My project/Assets/GameFolders/Scripts/ArrowsController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowsController : MonoBehaviour
+{
+    public List<GameObject> arrows = new List<GameObject>();
+    [SerializeField] GameObject _arrowPrefab;
+    public float mesafe;
+    public static ArrowsController Instance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateCircle();
+        Instance = this;
+    }
+
+    
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)){
+            var newArrow = Instantiate(_arrowPrefab,this.transform);
+            arrows.Add(newArrow);
+            CreateCircle();
+        }
+    }
+    
+    public void CreateCircle(){
+        var angle = 360 / arrows.Count;
+        for (int i = 0; i < arrows.Count; i++)
+        {
+            MoveArrow(arrows[i].transform, angle * i);
+        }
+    }
+    void MoveArrow(Transform arrow, float degree){
+        var pos = Vector3.zero;
+        pos.x = Mathf.Cos(degree * Mathf.Deg2Rad);
+        pos.y = Mathf.Sin(degree * Mathf.Deg2Rad);
+        arrow.transform.localPosition = pos * mesafe;
+    }
+}
